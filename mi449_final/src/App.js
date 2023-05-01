e8b56a7d5e8b028e9848842d87604a6821188479
Lauren Snyder <laurenxsnyder@gmail.com>
2023-04-30 23:43:14 -0400

    pictures

--- mi449_final/src/App.js
+++ mi449_final/src/App.js
@@ -42,6 +42,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Pictures />
         <img src={logo} className="App-logo" alt="logo" />
         
         
