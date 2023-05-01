fdb575dc93773271375321704114b46bd3f71b77
Lauren Snyder <laurenxsnyder@gmail.com>
2023-04-30 23:11:16 -0400

    fix

--- mi449_final/src/supabaseClient.js
+++ mi449_final/src/supabaseClient.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js' ;
 
 const supabaseUrl = 'https://pavinuwtygecdbnszgus.supabase.co' ;
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhdmludXd0eWdlY2RibnN6Z3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5MDExODIsImV4cCI6MTk5ODQ3NzE4Mn0.i2yHirifcnQJ7TeyvGHcLvP1LB3Dhvxo8bPS2T-4cDE ;
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhdmludXd0eWdlY2RibnN6Z3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5MDExODIsImV4cCI6MTk5ODQ3NzE4Mn0.i2yHirifcnQJ7TeyvGHcLvP1LB3Dhvxo8bPS2T-4cDE ;
 export const supabase = createClient(supabaseUrl, supabaseKey) ;
